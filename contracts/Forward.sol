f330187bf4dd92576d9de0e9e49acd4df7ba0e55
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-15 19:17:01 +0700

    :bug: Fix bugs

--- contracts/Forward.sol
+++ contracts/Forward.sol
@@ -2,36 +2,34 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract Forward {
   enum State { Ongoing, Inactive, Completed }
-  State public state;
+  struct ContractData {
+    string sender;
+    uint amount;
+    State state;
+  }
 
   uint contractCount = 0;
-  mapping (uint256 => ContractData) contracts;
-
-  struct ContractData {
-    uint amount;
-  }
+  ContractData[] public contracts;
 
   modifier condition(bool _condition) {
     require(_condition, "Invalid condition");
     _;
   }
 
-  modifier inState(State _state) {
-    require(state == _state, "Invalid state.");
-    _;
-  }
-
   event ContractEvent(uint amount);
 
-  function insert(uint _amount) public returns (uint256) {
-    ContractData memory newContract = ContractData(_amount);
-    contracts[contractCount] = newContract;
+  function insert(uint _amount, string memory _sender) public returns (uint rowNumber) {
+    ContractData memory newContract;
+    newContract.sender = _sender;
+    newContract.amount = _amount;
+    newContract.state = State.Ongoing;
+
+    contracts.push(newContract);
     contractCount++;
-    emit ContractEvent(_amount);
     return contractCount;
   }
 
-  function get(uint256 contractId) public view returns (uint amount) {
-    return (contracts[contractId].amount);
+  function get(uint256 id) public view returns (uint amount, string memory sender) {
+    return (contracts[id].amount, contracts[id].sender);
   }
 }
